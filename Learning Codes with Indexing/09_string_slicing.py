d5e17b3e2b5c9f54e6c40cfa4a254a882845ad6c
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-10-03 22:12:33 +0530

    First Character extraction code has been added

--- Learning Codes with Indexing/09_string_slicing.py
+++ Learning Codes with Indexing/09_string_slicing.py
@@ -1,4 +1,7 @@
 my_name = "Swapnaneel Ghosh"
 
-short_name = my_name[0:10]
+short_name = my_name[0:10] # Start from index 0 and end till 10th index including all teh letters in between
 print(f"The short name is: {short_name}")
+
+first_character = my_name[0]
+print(f"First Character: {first_character}")

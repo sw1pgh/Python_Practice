c9cb3d471d2446c75d39c45d06a7d4feeba1c658
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-10-12 23:31:58 +0530

    Practice Set 4 Question 1 has been fixed code-wise

--- Practice_Set_4/list_sort.py
+++ Practice_Set_4/list_sort.py
@@ -1,9 +1,13 @@
 # Python program to take 6 students number as input and print it in sorted order
 
 def enter_student_marks():
-    marks = [input(f"Enter {i + 1}st student's marks: ") for i in range(5)]
-    marks.sort()
-    print(f"Your students marks in sorted order are: {marks}")
+    marks = []
+    for i in range(0, 5):  # Changed to 6 to get marks for 6 students
+        mark = float(input(f"Enter #{i + 1} student's marks: "))  # Convert input to float
+        marks.append(mark)  # Append the mark directly to the list
+    
+    marks.sort()  # Sort the list of marks
+    print(f"Your students' marks in sorted order are: {marks}")
 
 if __name__ == "__main__":
     enter_student_marks()

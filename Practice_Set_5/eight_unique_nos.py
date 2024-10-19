ae3485aece38095bd7d14398ab59b8918a31e072
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-10-19 10:00:23 +0530

    Few code comments added

--- Practice_Set_5/eight_unique_nos.py
+++ Practice_Set_5/eight_unique_nos.py
@@ -1,5 +1,8 @@
 # Write a program to input 8 numbers from the User and print only the unique numbers
 
+# I have used a bit of OOP concept here. Where there are 2 functions/methods within the class which each return a specific thing.
+# Outside the class the main function is calling the necessary function by creating an instance of the class followed by the function name.
+
 class Unique_Numbers:
     
     def user_input(self):

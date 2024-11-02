3aa86b6e29aa3a4a2887fa90679e41202ad63d6d
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-02 21:00:17 +0530

    Function name changed

--- Practice_Set_6/num_existing_in_list.py
+++ Practice_Set_6/num_existing_in_list.py
@@ -1,6 +1,6 @@
 # WAP in Python to check whether an inputted number exists in the list or not
 
-def does_number_exist():
+def check_if_number_exist():
     number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     inputted_number = int(input("Please enter any number: "))  # Convert input to an integer
 
@@ -10,4 +10,4 @@ def does_number_exist():
         print("Number does NOT exist in our list.")
 
 if __name__ == "__main__":
-    does_number_exist()
+    check_if_number_exist()

d7fac88ece68df313758a0af5a41c0b74bb94151
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-04 20:57:25 +0530

    Some code comments added

--- Learning Codes with Indexing/23_for_continue.py
+++ Learning Codes with Indexing/23_for_continue.py
@@ -3,9 +3,9 @@ for i in range (1,90):
     print(i)
     
     if(i != 10):
-        continue
+        continue # Skips the iteration
     else:
-        break
+        break # Break this iteration
     
 # The logic here is that until i is 10, the iteration will continue but will break as soon as i = 10.
 # So it should print 1 to 10

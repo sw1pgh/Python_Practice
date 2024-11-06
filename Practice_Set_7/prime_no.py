66e67f1c2bce39a65c6904361cbf2bea878a73ad
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-06 21:37:04 +0530

    Prime Number Code Done

--- Practice_Set_7/prime_no.py
+++ Practice_Set_7/prime_no.py
@@ -1,1 +1,17 @@
 # WAP to find out whether a given number is prime or not
+
+def prime_check():
+    number = int(input("Please enter any number to check whether it is Prime or Not: "))
+    
+    if((number == 0) or (number == 1)):
+        print(f"{number} is NOT a Prime Number")
+    elif(number > 1):
+        for i in range(2, number):
+            if(number % i == 0):
+                print(f"{number} is NOT a prime number")
+                break
+        else:
+            print(f"{number} is a Prime Number")
+        
+if __name__ == "__main__":
+    prime_check()

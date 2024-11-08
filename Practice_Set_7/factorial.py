24f1d9a59fa33ee6aac4798db0da88e7529fb8d9
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-08 21:35:03 +0530

    Factorial Problem Solved

--- Practice_Set_7/factorial.py
+++ Practice_Set_7/factorial.py
@@ -1,1 +1,20 @@
-# WAP in Python to find the Factorial
+# WAP in Python to find the Factorial of a number using for loop
+
+def find_factorial():
+    num = int(input("Please enter any number: "))
+    factorial = 1
+    
+    if(num < 0):
+        print(f"Factorial Does NOT exist for Negative Numbers")
+        
+    elif(num == 0):
+        print(f"Factorial of {num} is: 1")
+        
+    else:
+        for i in range (1, num + 1):
+            factorial = factorial * i
+
+        print(f"The factorial of {num} is: {factorial}")
+
+if __name__ == "__main__":
+    find_factorial()

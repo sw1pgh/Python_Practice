9a553166515df688ce4a8e8c90621a5a9746589b
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-16 18:42:47 +0530

    Star Pattern One code modified

--- Practice_Set_7/star_1.py
+++ Practice_Set_7/star_1.py
@@ -10,8 +10,7 @@ def star_pattern_one():
     n = int(input("Please enter the Limit of the Pattern: "))
     for i in range (1, n + 1):
         print(" " * (n - i), end = "")
-        print("*" * (2 * i - 1), end = "")
-        print("")
+        print("*" * (2 * i - 1))
     
 if __name__ == '__main__':
     star_pattern_one()

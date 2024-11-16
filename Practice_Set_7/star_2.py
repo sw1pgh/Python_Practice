5f017af1ab98ad844aadb4d6df4f655504167e28
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-11-16 18:45:25 +0530

    Star Pattern Two Done

---
+++ Practice_Set_7/star_2.py
@@ -0,0 +1,15 @@
+# WAP to print the following pattern:
+'''
+*
+**
+***
+'''
+
+def star_pattern_two():
+    
+    n = int(input("Please enter the Limit of the Pattern: "))
+    for i in range (1, n + 1):
+        print("*" * (i))
+    
+if __name__ == '__main__':
+    star_pattern_two()

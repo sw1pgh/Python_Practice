666211c49169933649fc25abe29095138ad16d5e
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-02 21:05:56 +0530

    Function with return value code done

---
+++ Learning Codes with Indexing/26_variable_func_return.py
@@ -0,0 +1,8 @@
+# WAP to write a function in Python which will return a value. Store the entire function call in a variable and print it.
+
+def function_with_value():
+    print("Hello! This function returns the following value:-")
+    return 180
+
+variable_a = function_with_value()
+print(f"{variable_a}")

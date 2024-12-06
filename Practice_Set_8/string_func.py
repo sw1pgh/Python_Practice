9243877cbfc89a5ddb3c271b591f3886022ccf7c
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-06 22:29:27 +0530

    Code modified with a bit of class concept

--- Practice_Set_8/string_func.py
+++ Practice_Set_8/string_func.py
@@ -1,16 +1,17 @@
 # WAP in Python to create a function to remove a given word from a list and strip it at the same time
 
-def remove_word(input_word):
-    try:
-        list_words = ["Apple", "Banana", "Cherry", "Avocado", "Pear", "Tomato"]
-        list_words.remove(input_word)
-        print(f"These updated list of fruits is :" '\n' f"{list_words}")
-        
-    except:
-        print(f"{input_word} does NOT exist in the List")
+class Fruits:
+    list_words = ["Apple", "Banana", "Cherry", "Avocado", "Pear", "Tomato"]
+    
+    def remove_word(self, input_word):
+        try:
+            Fruits().list_words.remove(input_word)
+            print(f"These updated list of fruits is :" '\n' f"{Fruits().list_words}")
+            
+        except:
+            print(f"{input_word} does NOT exist in the List")
     
 if __name__ == "__main__":
-    list_words = ["Apple", "Banana", "Cherry", "Avocado", "Pear", "Tomato"]
-    print(f"These are the following list of fruits:" '\n' f"{list_words}")
+    print(f"These are the following list of fruits:" '\n' f"{Fruits().list_words}")
     inp_word = input("Enter the fruit's name that you want to remove: ")
-    remove_word(inp_word)
+    Fruits().remove_word(inp_word)

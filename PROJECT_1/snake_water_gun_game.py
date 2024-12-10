1b6b4f72b28b15f17c4df29c7656917fc9b967ad
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-10 08:47:32 +0530

    Readme Updated

--- PROJECT_1/snake_water_gun_game.py
+++
@@ -1,5 +0,0 @@
-'''
-We all have played snake, water, gun game in our childhood.
-If you haven't, google the rules of this game and
-Write A Python Program capable of playing this game with the User
-'''

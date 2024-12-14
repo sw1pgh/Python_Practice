4e6ee0d8afa019c5fb719a575d2308d1f016e73d
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-14 09:01:26 +0530

    Project 1 Updated

--- PROJECT_1/rock_paper_scissors.py
+++ PROJECT_1/rock_paper_scissors.py
@@ -11,22 +11,21 @@ import random
 
 class Rock_Papaer_Scissors_Game:
     
-    def get_random_choice_from_computer():
+    def get_random_choice_from_computer(self):
         choices = ["rock", "paper", "scissors"]
         return random.choice(choices)
     
-    def print_statements():
-        print(f''''
-            The rock is a closed fist;
-            paper is a flat hand with fingers and thumb extended and the palm facing downward;
-            and scissors is a fist with the index and middle fingers fully extended toward the opposing player.
-            Rock wins against Scissors;
-            Paper wins against Rock; and
-            Scissors wins against Paper.
+    def print_statements(self):
+        print(f'''
+            :RULES:
+                        
+            -> Rock wins against Scissors;
+            -> Paper wins against Rock; and
+            -> Scissors wins against Paper.
             ''')
         print("")
         
-    def get_user_input():
+    def get_user_input(self):
         print("Enter your choice:")
         print("1 - rock")
         print("2 - paper")
@@ -46,17 +45,26 @@ class Rock_Papaer_Scissors_Game:
             except ValueError:
                 print("Invalid input, please enter a number.")
                 
-    def determine_winner(user_choice, computer_choice):
+    def determine_winner(self, user_choice, computer_choice):
         # Rules of Rock, Paper, Scissors
         if user_choice == computer_choice:
-            return "It's a tie!"
+            print("It's a tie!")
         elif (user_choice == "rock" and computer_choice == "scissors") or \
              (user_choice == "paper" and computer_choice == "rock") or \
              (user_choice == "scissors" and computer_choice == "paper"):
-                return "You win!"
+                print("You win!") 
         else:
-            return "You lose!"
+            print("You lose!")
         
     
-    user_choice = get_user_choice()
-    computer_choice = get_random_choice()
+if __name__ == "__main__":
+    
+    r_p_s = Rock_Papaer_Scissors_Game()
+    
+    r_p_s.print_statements()
+    
+    computer_choice = r_p_s.get_random_choice_from_computer()
+    user_choice = r_p_s.get_user_input()
+    
+    r_p_s.determine_winner(user_choice, computer_choice)
+    

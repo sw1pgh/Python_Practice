c6bf2e682ae34543fc703f409e2b649e005294e2
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-16 18:02:46 +0530

    Code comment added for better understanding

--- Learning Codes with Indexing/28_file_opening.py
+++ Learning Codes with Indexing/28_file_opening.py
@@ -1,4 +1,4 @@
-f = open("Learning Codes with Indexing/file.md")
-data = f.read()
-print(data)
-f.close()
+f = open("Learning Codes with Indexing/file.md") # Opens up the file from the directory
+data = f.read() # Reads the file
+print(data) # Prints the data from the file
+f.close() # CLoses the file after reading

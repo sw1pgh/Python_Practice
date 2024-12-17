c48837d2cffc1d24ccf5ac31eebd39dcf75b5e87
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2024-12-17 21:56:46 +0530

    Tried r + method of file reading

--- Learning Codes with Indexing/29_file_writing.py
+++ Learning Codes with Indexing/29_file_writing.py
@@ -1,6 +1,6 @@
 editable_string = "Hello! I am Swap"
 
-f = open("Learning Codes with Indexing/file.md", "w+")
+f = open("Learning Codes with Indexing/file.md", "r+")
 f.write(editable_string)
 print(f)
 f.close()

42bf4c7dec63a3fb8ed0af02e618d91df5cb5ad0
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-01-04 16:52:21 +0530

    Example code done on class and object

---
+++ Learning Codes with Indexing/31_c_and_o.py
@@ -0,0 +1,10 @@
+# Learning Class and Object in Python
+
+class Employee: # Class
+    name = "Swap"
+    hobby = "Watching Football"
+    title = "Ghosh"
+    
+obj_emp = Employee() # Instantiation of the Class -> Object
+
+print(f"Name = {obj_emp.name}")

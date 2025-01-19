8952a2d7f63356d26b59e81afc40625199755ee7
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-01-19 09:59:04 +0530

    Code Updated

--- Learning Codes with Indexing/31_c_and_o.py
+++ Learning Codes with Indexing/31_c_and_o.py
@@ -5,10 +5,10 @@ class Employee: # Class
     hobby = "Watching Football"
     title = "Ghosh"
     
-    # Here [name, hobby, title] are the attributes of the class Employee
+    # Here [name, hobby, title] are the attributes of the class Employee --> Class attribute
     
 obj_emp = Employee() # Instantiation of the Class -> Object
-obj_emp.laptop = "M2 MBA"
+obj_emp.laptop = "M2 MBA" # Here laptop is the object attribute
 
 print(f"Name = {obj_emp.name}\nLaptop = {obj_emp.laptop}")
 

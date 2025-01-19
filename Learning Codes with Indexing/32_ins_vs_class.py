6e66bad796c062edb53338bd1dd0856a23fdf2d1
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-01-19 10:24:56 +0530

    New function greet() has been added to example code

--- Learning Codes with Indexing/32_ins_vs_class.py
+++ Learning Codes with Indexing/32_ins_vs_class.py
@@ -4,6 +4,9 @@ class Employee():
     
     def getInfo(self):
         print(f"The language is {self.language} and the name is {self.name}")
+        
+    def greet(self):
+        print("Good Morning!")
     
 obj_emp = Employee()
 obj_emp.language = "JavaScript"
@@ -13,5 +16,6 @@ Now when obj_emp.language will be printed then JavaScript will be printed instea
 as Instance/Object Attributes takes preference over Class Attribute
 '''
 
-print(f"Language = {obj_emp.language}")
+# print(f"Language = {obj_emp.language}")
+obj_emp.greet()
 obj_emp.getInfo()

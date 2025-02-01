2fc50bac1ca6922fd1a7e52486d3ca5c49fd4e10
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-02-01 11:05:59 +0530

    Problem 4 of PS 10 is Done

---
+++ Practice_Set_10/modify_problem_2.py
@@ -0,0 +1,17 @@
+# Modify Problem 2 of PS 10 to include a static method to greet the User with Hello
+
+class Class:
+    a = "Hello Citizen!"
+    
+    @staticmethod # decorator has been used to mark greet_user() as a static method
+    def greet_user():
+        print("Hello! Welcome to Python Programming...🥳")
+    
+OBJECT = Class()
+
+OBJECT.a = 0
+
+OBJECT.greet_user()
+
+print(f"Value of a = {OBJECT.a}")
+

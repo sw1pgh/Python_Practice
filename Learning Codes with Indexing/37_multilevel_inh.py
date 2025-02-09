8f9c642c92644b56371583afa177f4606503d241
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-02-09 17:29:18 +0530

    Code altered

--- Learning Codes with Indexing/37_multilevel_inh.py
+++ Learning Codes with Indexing/37_multilevel_inh.py
@@ -1,10 +1,14 @@
 # Multilevel Inheritance
 
 class Coder():
-    pass
+    a = 1
 
 class Employee(Coder):
-    pass
+    b = 2
 
 class Manager(Employee):
-    pass
+    c = 3
+
+man_obj = Manager()
+sum = man_obj.a + man_obj.b + man_obj.c
+print(f"Sum = {sum}")

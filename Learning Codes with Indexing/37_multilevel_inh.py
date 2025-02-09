95cc1123315be693c52d7a9e60d8557d60193121
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-02-09 17:38:32 +0530

    Super class learnt

--- Learning Codes with Indexing/37_multilevel_inh.py
+++ Learning Codes with Indexing/37_multilevel_inh.py
@@ -1,13 +1,28 @@
 # Multilevel Inheritance
 
 class Coder():
+    
+    def __init__(self):
+        print("Constructor of Coder class")
+    
     a = 1
 
 class Employee(Coder):
+    
+    def __init__(self):
+        print("Constructor of Employee class")
+    
     b = 2
 
 class Manager(Employee):
+    
+    def __init__(self):
+        print("Constructor of Coder class")
+        super().__init__() # Super class to print the constructors of the two previous classes as well
+    
     c = 3
+
+# Each Individual Class' object will print that respective class' constructor
 
 man_obj = Manager()
 sum = man_obj.a + man_obj.b + man_obj.c

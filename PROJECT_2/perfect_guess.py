25325d65d3dec3a7b7f439b6b747a62ec9d0e69d
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-02-23 08:56:56 +0530

    Project 2 Initialized

---
+++ PROJECT_2/perfect_guess.py
@@ -0,0 +1,7 @@
+# WAP that generates a random number a asks the user to guess it.
+'''
+-> If the player's guess is higher than the actual number, the program displays "Lower number please"
+-> If the User's guess is too low, the program displays "Higher Number Please"
+-> When the User guesses the correct number, the program displays the number of guesses the player used to arrive at the number
+'''
+

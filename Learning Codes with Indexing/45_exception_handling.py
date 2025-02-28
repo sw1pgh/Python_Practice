7d2c8012f587e78ba89d285ab2a1beced707a9dd
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-02-28 22:40:41 +0530

    Code Updated

--- Learning Codes with Indexing/45_exception_handling.py
+++ Learning Codes with Indexing/45_exception_handling.py
@@ -4,6 +4,11 @@ def user_input_integer():
     try:
         ui = int(input("Please enter an integer number only: "))
         print(f"Thanks for entering an Integer")
+        
+    except ValueError as v:
+        print(v)
+        user_input_integer()
+        
     except Exception as e:
         print(e)
         print(f"I said only INTEGER!!!!")

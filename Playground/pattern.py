ef2de9d38679d81ce2bea3af3c4899d401905713
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-09 08:42:22 +0530

    Prob 1 of Python Playgrounf done

--- Playground/pattern.py
+++ Playground/pattern.py
@@ -9,6 +9,6 @@
 length = int(input("Please enter the length as an integer: "))
 stars = 1
 
-for i in range (1, length):
-    print(" " * (2 * length - i), "*" * (stars))
+for i in range (1, length + 1):
+    print(" " * (length - i), "*" * (stars))
     stars += 2

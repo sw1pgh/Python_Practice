3ebacc8797cb761ced7bcc4cf94a549c26744289
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-10 21:05:45 +0530

    Needle in Haystack Done

---
+++ Playground/needle_in_haystack.py
@@ -0,0 +1,6 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return print(haystack.find(needle))
+
+Solution().strStr(haystack = "leetcode", needle = "leeto")
+Solution().strStr(haystack = "sadbutsad", needle = "sad")

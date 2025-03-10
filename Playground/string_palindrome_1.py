b570db1d65b9b8956c004120fdcca7734ff343e0
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-10 21:29:03 +0530

    String Palindrome 1st Method Done

--- Playground/string_palindrome_1.py
+++ Playground/string_palindrome_1.py
@@ -2,13 +2,6 @@ class Solution:
     def strPal(self, input_str: str) -> bool:
         
         rev_str = input_str[::-1]
-     
-        li_1 = [1,4,5,8,9,111]
-        
-        li_2 = li_1[::-1]
-        print(li_2)
-        
         return print(rev_str.lower() == input_str.lower())
-    
-    
+
 Solution().strPal('Dad')

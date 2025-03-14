98451ac28638cb177ebfd551bff422759b1708a1
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-14 19:31:37 +0530

    First Flask Demo Project Done

---
+++ Practice_Set_13/app.py
@@ -0,0 +1,9 @@
+# Explore the 'flask' module and create a web-server using Flask & Python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"

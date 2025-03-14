6c4016b3eb16454d22b8e986b3b1691b7668aaef
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-14 18:45:01 +0530

    Code Updated

--- playwright_practice/conftest.py
+++ playwright_practice/conftest.py
@@ -5,43 +5,20 @@ from playwright.sync_api import sync_playwright
 def playwright_instance():
     with sync_playwright() as launcher:
         yield launcher
-        
-@pytest.fixture(scope="class", params = ["chromium", "firefox", "webkit"])
+
+@pytest.fixture(scope="class", params=["chromium", "firefox", "webkit"])
 def browser_context(playwright_instance, request):
-    browser = getattr(playwright_instance, request.param).launch(headless = True, args = ["--start-maximized"])
+    browser = getattr(playwright_instance, request.param).launch(headless=True, args=["--start-maximized"])
     context = browser.new_context()
     
     yield context
     
     context.close()
-    browser.close()    
-    
+    browser.close()
+
 @pytest.fixture(scope="class")
 def page(browser_context):
     page = browser_context.new_page()
+    
     yield page
     page.close()
-
-
-
-# @pytest.fixture(scope="session")
-# def playwright_instance():
-#     with sync_playwright() as launcher:
-#         yield launcher
-
-# @pytest.fixture(scope="class", params=["chromium", "firefox", "webkit"])
-# def browser_context(playwright_instance, request):
-#     browser = getattr(playwright_instance, request.param).launch(headless=True, args=["--start-maximized"])
-#     context = browser.new_context()
-    
-#     yield context
-    
-#     context.close()
-#     browser.close()
-
-# @pytest.fixture(scope="class")
-# def page(browser_context):
-#     page = browser_context.new_page()
-    
-#     yield page
-#     page.close()

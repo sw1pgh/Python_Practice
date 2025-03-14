bd3ed54e6dd8ec6da2ae7e676e482ae13c2db107
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-14 18:21:39 +0530

    Code Updated

--- Playground/2_sum.py
+++ Playground/2_sum.py
@@ -1,1 +1,14 @@
 # Two Sums
+
+from typing import List
+
+class Solution:
+    def twoSums(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i, num in enumerate(nums):
+            desired_value = target - num
+            if desired_value in hash_map:
+                return [hash_map[desired_value], i]
+            hash_map[num] = i
+
+print(Solution().twoSums(nums = [10,10,10], target = 20))

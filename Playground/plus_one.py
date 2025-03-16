807830afea04fcc260698fc33169fda73775f865
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-03-16 08:23:25 +0530

    Plus One problem Done

--- Playground/plus_one.py
+++ Playground/plus_one.py
@@ -3,6 +3,6 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]):
         num = int("".join(map(str, digits)))
-        return print([int(digit) for digit in str(num + 1)])
+        return [int(digit) for digit in str(num + 1)]
         
-Solution().plusOne(digits = [4,3,2,1])
+Solution().plusOne(digits = [1,2,3])

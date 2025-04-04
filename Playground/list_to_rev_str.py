d47157eecc0a666a02a3ede8563432abfc227db5
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-04-04 18:22:57 +0530

    Code Updated

--- Playground/list_to_rev_str.py
+++ Playground/list_to_rev_str.py
@@ -5,7 +5,6 @@ from typing import List
 class Rev:
     def list_2_rev_str(self, nums: List[str]) -> str:
         rev_str = list(reversed(''.join(nums)))
-        print(type(str(rev_str)))
-        return rev_str
+        return str(rev_str)
     
 print(Rev().list_2_rev_str(nums = ['1,2,3,4,5,6']))

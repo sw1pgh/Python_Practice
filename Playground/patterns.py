e80d02fd366b6f2480bcbccbfc5342c70d48f560
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2026-01-25 20:17:51 +0530

    Christmas Tree

--- Playground/patterns.py
+++ Playground/patterns.py
@@ -1,10 +1,14 @@
 class Patterns:
     def pattern(self):
         n = int(input("Please enter the number of lines: "))
-        o = n
-        for i in range(1,n+1):
-            print("* " * o)
-            o = o-1
-        print("\n")
+        for i in range(1, n + 1):
+            print(" " * (n - i) + "* " * i)
 
 Patterns().pattern()
+
+
+# def pat(self):
+#     n = int(input("Please enter the number of lines: "))
+#     for i in range(1,n+1):
+#             print("* " * i)
+#             print("")

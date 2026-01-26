d29bee21c36780a605a12773a5d0878be22fcda1
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2026-01-26 11:07:10 +0530

    Patterns Learnt

--- Playground/patterns.py
+++ Playground/patterns.py
@@ -1,14 +1,24 @@
 class Patterns:
-    def pattern(self):
+    
+    def pattern_1(self):
         n = int(input("Please enter the number of lines: "))
         for i in range(1, n + 1):
             print(" " * (n - i) + "* " * i)
 
-Patterns().pattern()
+    def pattern_2(self):
+        n = int(input("Please enter the number of lines: "))
+        
+        # Top half (straight)
+        for i in range (1, n+1):
+            print(" " * (n - i) + "* " * i)
+            
+        # Bottom half (reverse)
+        for i in range(n - 1, 0, -1):
+            print(" " * (n - i) + "* " * i)
+            
+    def pattern_3(self):
+        n = int(input("Please enter the number of lines: "))
+        for i in range(n, 0, -1):
+            print("* " * i)
 
-
-# def pat(self):
-#     n = int(input("Please enter the number of lines: "))
-#     for i in range(1,n+1):
-#             print("* " * i)
-#             print("")
+Patterns().pattern_3()
